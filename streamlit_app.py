32de8dc94c9e0c6b0a7b3baca6a83dbabe8eef4d
wilburn-projects <chris.wilburn.2077@gmail.com>
2025-12-08 15:11:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,7 +188,7 @@ def annotate_chart(base_chart, events):
         - Designed for time series plots with an 'ArrivalYear' axis.
     """
     # staggered offsets for readability
-    offsets = [-20, -45, -70, -90, -110, -130]
+    offsets = [-20, -40, -60, -80, -100, -120]
     layers = [base_chart]
 
     if not events:
